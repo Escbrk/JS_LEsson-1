85da3dca7b4d98d888e262ea25c6145dda1890e3
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-11-07 00:34:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3864,7 +3864,7 @@ const propertyName = 'tracks';
 // console.log('objA', objA);
 
 /*
- *      Основы ООП: классб экземпляр (объект), интерфейс
+ *      Основы ООП: класс, экземпляр (объект), интерфейс
  */
 
 /*
@@ -3874,3 +3874,172 @@ const propertyName = 'tracks';
  *          - Свойство Function.prototype
  */
 
+//!     1. Если функция вызывается через new, создаётся пустой объект
+//!     2. Функция вызывается в контексте созданного объекта,
+//!         то есть в this записывается ссылка на него
+//!     3. В свойство this.__proto__ записывается ссылка на объект Car.prototype
+//!         то есть Car.prototype это ПРОТОТИП будущего объекта (экземпляра)
+//!     4. Ссылка на объект возвращается в место вызова new Car
+
+// const Car = function ({ brand, model, price } = {}) {
+//     // const { brand, model, price } = config;
+
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+
+// };
+
+// Car.prototype.sayHi = function () {
+//     console.log('Car.prototype.sayHi -> this', this);
+//     console.log('Hello :) ');
+// }
+
+// Car.prototype.changePrice = function (newPrice) {
+//         this.price = newPrice;
+//     };
+
+// console.log(Car.prototype);
+
+// const myCar = new Car({
+//     brand: 'Audi',
+//     model: 'Q3',
+//     price: 35000,
+// });
+// console.log(myCar);
+// myCar.sayHi();
+// myCar.changePrice(10000)
+// console.log(myCar);
+
+// const myCar2 = new Car({
+//     brand: 'BMW',
+//     model: 'Q3',
+//     price: 50000,
+// });
+// console.log(myCar2);
+
+// const myCar3 = new Car({
+//     brand: 'Audi',
+//     model: 'A6',
+//     price: 65000,
+// });
+// console.log(myCar3);
+
+//? ___________________________________________
+
+const User = function ({ email, password } = {}) {
+    (this.email = email), (this.password = password);
+};
+
+User.prototype.changeEmail = function (newEmail) {
+    this.email = newEmail;
+};
+User.prototype.changePassword = function (newPassword) {
+    this.password = newPassword;
+};
+
+const mango = new User({ email: 'mango@mail.com', password: 1111111 });
+// console.log(mango);
+
+mango.changeEmail('pahancheg@icloud.com');
+// console.log(mango);
+
+mango.changePassword('qweqwe123');
+
+// console.log(mango);
+
+//? ___________________________________________
+
+//!     1. У каждого объекта есть свойство __proto__
+//!     2. В этом свойстве лежит ссылка на его ПРОТОТИ, то есть другой объект
+//!     3. При создании литерала объекта, в свойство __proto__ записывается ссылка на Функция.prototype
+
+// const objA = {
+//     x: 5,
+// };
+
+// console.log(objA.__proto__ === Object.prototype);    //! true
+
+//!     4. Функция-конструктор это просто функция :)
+//!     5. Всю магию делает оператор new
+//!     6. Если функция вызывается через new, создаётся пустой объект
+//!     7. Функция вызывается в контексте созданного объекта
+//!     8. В свойство this.__proto__ записывается ссылка на объект Функция.prototype
+//      this = Object.create(User.prototype)
+
+//!     9. Ссылка на объект возвращается в место вызова new Функция()
+
+//? ___________________________________________
+
+/*
+ *      Статические свойства и методы
+ *          - Статические свойства и методы доступны только на самом конструкторе
+ *          - В статических методах не нужен this
+ */
+
+// User.message = 'Я статическое свойство, меня нет на экземплярах или в прототипах'
+
+// User.logInfo = function (obj) {
+//     console.log('User.logInfo -> obj', obj);
+//     console.log('Почта: ', obj.email);
+//     console.log('Пароль: ', obj.password);
+
+// }
+
+// User.logInfo(mango)
+
+// console.dir(User);
+// console.log(Math.round(5.1));
+// console.log(Math.PI);
+
+//? ___________________________________________
+
+// const CounterPlugin = function ({
+//     rootSelector,
+//     initialValue = 0,
+//     step = 1,
+// } = {}) {
+//     this._value = initialValue;
+//     this._step = step;
+
+//     this._refs = this._getRefs(rootSelector);
+//     this._bindEvents();
+//     this.updateValueUI()
+// };
+// CounterPlugin.prototype._getRefs = function (rootSelector) {
+//     const refs = {};
+//     refs.container = document.querySelector(rootSelector);
+//     refs.incrementBtn = refs.container.querySelector('[data-increment]');
+//     refs.decrementBtn = refs.container.querySelector('[data-decrement]');
+//     refs.value = refs.container.querySelector('[data-value]');
+
+//     return refs;
+// };
+
+// CounterPlugin.prototype._bindEvents = function () {
+//     this._refs.incrementBtn.addEventListener('click', () => {
+//         this.increment();
+//         this.updateValueUI();
+//     });
+
+//     this._refs.decrementBtn.addEventListener('click', () => {
+//         this.decrement();
+//         this.updateValueUI();
+//     });
+// };
+
+// CounterPlugin.prototype.updateValueUI = function () {
+//     this._refs.value.textContent = this._value;
+// };
+
+// CounterPlugin.prototype.increment = function () {
+//     this._value += this._step;
+// };
+// CounterPlugin.prototype.decrement = function () {
+//     this._value -= this._step;
+// };
+
+// new CounterPlugin({ rootSelector: '#counter1', step: 10, initialValue: 100 });
+// new CounterPlugin({ rootSelector: '#counter2', step: 2 });
+
+//? ___________________________________________
