6e7c22d83189cc818a5408f80b5ba574eb3e45de
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-11-26 10:44:21 -0700

    Update createModal.js

--- src/helpers/createModal.js
+++ src/helpers/createModal.js
@@ -1,41 +1,41 @@
-import * as basicLightbox from 'basiclightbox';
-import 'basiclightbox/dist/basiclightbox.min.css';
+// import * as basicLightbox from 'basiclightbox';
+// import 'basiclightbox/dist/basiclightbox.min.css';
 
-function createModal(product) {
-  const instance = basicLightbox.create(
-    `
-        <div style='width: 600px; background-color: white;'>
-            <img src='${product.img}' alt='${product.name}'>
-            <h2>${product.name}</h2>
-            <h3>${product.price} $</h3>
-            <p>${product.description}</p>
+// function createModal(product) {
+//   const instance = basicLightbox.create(
+//     `
+//         <div style='width: 600px; background-color: white;'>
+//             <img src='${product.img}' alt='${product.name}'>
+//             <h2>${product.name}</h2>
+//             <h3>${product.price} $</h3>
+//             <p>${product.description}</p>
             
-            <div>
-                <button class='js-favorite'>Add to favorite</button>
-                <button class='js-cart'>Add to cart</button>
-            </div>
+//             <div>
+//                 <button class='js-favorite'>Add to favorite</button>
+//                 <button class='js-cart'>Add to cart</button>
+//             </div>
 
-        </div>
-      `,
-    {
-      handler: null,
-      onShow(instance) {
-        this.handler = closeModal.bind(instance)
-        document.addEventListener('keydown', this.handler)
-      },
+//         </div>
+//       `,
+//     {
+//       handler: null,
+//       onShow(instance) {
+//         this.handler = closeModal.bind(instance)
+//         document.addEventListener('keydown', this.handler)
+//       },
 
-      onClose() {
-        document.removeEventListener('keydown', this.handler);
-      },
-    });
+//       onClose() {
+//         document.removeEventListener('keydown', this.handler);
+//       },
+//     });
 
-  instance.show();
-}
+//   instance.show();
+// }
 
-function closeModal(evt) {
-  if (evt.code === 'Escape') {
-    this.close()
-  }
-}
+// function closeModal(evt) {
+//   if (evt.code === 'Escape') {
+//     this.close()
+//   }
+// }
 
-export { createModal };
+// export { createModal };
