5352a5424f87e5c5d86510758bfefc75542b6d35
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-11-27 18:13:33 -0700

    update

--- src/js/index.js
+++ src/js/index.js
@@ -5547,7 +5547,14 @@ const refs = {
   month: document.querySelector('.date-month'),
   year: document.querySelector('.date-year'),
   digitalClock: document.querySelector('.date-time'),
+
+  //!____________________________
+
+  clockHours__arrow: document.querySelector('.clock-hours__arrow'),
+  clockMinutes__arrow: document.querySelector('.clock-minutes__arrow'),
+  clockSeconds__arrow: document.querySelector('.clock-seconds__arrow'),
 };
+
 
 const namesOfMonth = [
   'Январь',
@@ -5586,7 +5593,18 @@ setInterval(() => {
     minutes: currentDate.getMinutes().toString().padStart(2, 0),
     seconds: currentDate.getSeconds().toString().padStart(2, 0),
   };
-    const formatTime = `${currentTime.hours}:${currentTime.minutes}:${currentTime.seconds}`;
 
-    refs.digitalClock.textContent = formatTime
+  const clock = {
+    // hours
+    minutes: (360 / 60) * currentTime.minutes,
+    seconds: (360 / 60) * currentTime.seconds,
+  };
+
+  // console.log(clock.minutes)
+  const formatTime = `${currentTime.hours}:${currentTime.minutes}:${currentTime.seconds}`;
+
+  refs.digitalClock.textContent = formatTime;
+
+  refs.clockSeconds__arrow.style.transform = `rotate(${clock.seconds}deg)`;
+  // refs.clockMinutes__arrow.style.transform = `rotate(${clock.minutes}deg)`;
 }, 1000);
