afde661c8af0df93cc49a2758fbaf22bc583b285
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-12-27 04:22:41 +0000

    Deploying to gh-pages from @ Escbrk/JS_Lesson-1@058a2e896ce08441841d0a9e916fa9df43b19ef4 🚀

---
+++ index.37e6ce63.js
@@ -0,0 +1,2 @@
+({notification:document.querySelector(".js-alert")}).notification.addEventListener("click",function(){});
+//# sourceMappingURL=index.37e6ce63.js.map
---
+++ index.5f775a8a.js
@@ -0,0 +1,2 @@
+({notification:document.querySelector(".js-alert")}).notification.addEventListener("click",function(){});
+//# sourceMappingURL=index.5f775a8a.js.map
