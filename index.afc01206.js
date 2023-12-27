17e22bb3b0c9b56a7df0d2447333ae03496bf077
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-12-27 01:20:15 +0000

    Deploying to gh-pages from @ Escbrk/JS_Lesson-1@bf3e1bf7f392e2a55b78e80d9ec76a91dbf6ef00 🚀

---
+++ index.afc01206.js
@@ -0,0 +1,2 @@
+console.log("До вызова setInterval");const o=setInterval(o=>console.log(`\u{41B}\u{43E}\u{433} \u{43A}\u{430}\u{436}\u{434}\u{44B}\u{435} ${o}ms - ${Date.now()}`),2e3,2e3);console.log("После вызова setInterval");const e=Math.random()>.3;console.log(e),e&&clearInterval(o);
+//# sourceMappingURL=index.afc01206.js.map
---
+++ index.ff0448b3.js
@@ -0,0 +1,2 @@
+!function(){console.log("До вызова setInterval");let e=setInterval(e=>console.log(`\u{41B}\u{43E}\u{433} \u{43A}\u{430}\u{436}\u{434}\u{44B}\u{435} ${e}ms - ${Date.now()}`),2e3,2e3);console.log("После вызова setInterval");let l=Math.random()>.3;console.log(l),l&&clearInterval(e)}();
+//# sourceMappingURL=index.ff0448b3.js.map
